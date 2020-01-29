54c68b6865342d56ff275804978614e4956a00aa
scruffythejanitor <pemberton5150@gmail.com>
2020-01-28 22:00:40 -0700

    updated Javascript

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,12 +1,17 @@
 $(document).ready(function () {
     var playerScore = 0;
     var gameRunning = false;
-    var questions = ["How many licks does it take to get to the center of a tootsie pop?"]
+    var questions = ["How many licks does it take to get to the center of a tootsie pop?", "two", "three"]
     var answers = {0:["1, TAHOO, 3, **Crunch**", "94", "651","364"], 1:["yes", "no", "maybe", "so"]}
+    var correctAnswers = ["3",]
+    var userAnswers = []
     var questionStep = 0
     var answerStep = 0
+    var guessedCorrect = 0
+    var guessedWrong = 0
 
     console.log(answers[1][0]);
+    console.log(userAnswers);
     welcomeScreen()
     function welcomeScreen() {
         $('.game').html("<h1>Welcome!</h1><br><h2>Click To Get Started</h2>");
@@ -23,15 +28,23 @@ $(document).ready(function () {
             questionForm()
             $('.one').on('click', function () {
                 console.log('button one was clicked');
+                questionStep++
+                userAnswers.push("0")
               })
             $('.two').on('click', function () {
                 console.log('button two was clicked');
+                questionStep++
+                userAnswers.push("1")
               })
             $('.three').on('click', function () {
                 console.log('button three was clicked');
+                questionStep++
+                userAnswers.push("2")
               })
             $('.four').on('click', function () {
                 console.log('button four was clicked');
+                questionStep++
+                userAnswers.push("3")
               })
         }
     })
@@ -39,10 +52,10 @@ $(document).ready(function () {
     function questionForm() {
         $('.question').text(questions[questionStep])
         $('.game').html('<button class="btn one">Button One</button>' 
-        + '<button class="btn two"></button>' 
+        + '<button class="btn two">Button One</button>' 
         + '<button class="btn three">Button three</button>' 
         + '<button class="btn four">Button four</button>');
-        questionStep++
+        
     }
 
 
